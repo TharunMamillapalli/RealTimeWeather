b8988256925cabcf946ae7a40062dfcd0dc58a0c
tharun <tharunmamillapalli@gmail.com>
2025-01-07 22:42:37 -0600

    Initial Commit

---
+++ app/src/main/java/com/example/realtimeweather/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.realtimeweather
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModelProvider
+import com.example.realtimeweather.ui.theme.RealTimeWeatherTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val weatherViewModel= ViewModelProvider(this)[WeatherViewModel::class.java]
+
+        setContent {
+            WeatherPage(weatherViewModel)
+
+        }
+    }
+}
+
+
---
+++ app/src/main/java/com/example/realtimeweather/api/Constant.kt
@@ -0,0 +1,5 @@
+package com.example.realtimeweather.api
+
+object Constant {
+    val apiKey="c1c66cbe3b374c33813213402250701"
+}
